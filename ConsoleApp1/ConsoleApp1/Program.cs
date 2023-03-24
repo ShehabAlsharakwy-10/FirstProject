b43a715986f0ccbe1a64a20b04c3ed4329d3358a
ShehabAlsharakwy-10 <126274547+ShehabAlsharakwy-10@users.noreply.github.com>
2023-03-24 23:39:59 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Task
+{
+
+    class car
+    {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public double Price { get; set; }
+    public bool IsElectric { get; set; }
+    public int NumDoors { get; set; }
+    public string Color { get; set; }
+    public double FuelEfficiency { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine("Starting the car...");
+    }
+
+    public void Accelerate()
+    {
+        Console.WriteLine("Accelerating the car...");
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Braking the car...");
+    }
+
+    public double CalculateMpg(double milesDriven, double gallonsUsed)
+    {
+        return milesDriven / gallonsUsed;
+    }
+
+    public void Paint(string color)
+    {
+        Color = color;
+        Console.WriteLine($"The car has been painted {color}.");
+    }
+
+    public bool IsAffordable(double budget)
+    {
+        return Price <= budget;
+    }
+
+    }
+    internal class Program
+    {
+    public static void Main(string[] args)
+    {
+        car c = new car();
+        c.Make = "";
+        c.Model = "";
+        c.Year = 2017;
+        c.Price = 500000.0;
+        c.IsElectric = false;
+        c.NumDoors = 4;
+        c.Color = "Black";
+        c.FuelEfficiency = 0.0;
+    }
+    }
+}
